b3cb70da449046fbca70f930c3debdb4c2763d6d
Arma971 <hperb0849@gmail.com>
2023-02-07 21:50:30 +0100

    script

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var divs = document.getElementsByTagName('span');
+for(var i=0; i<divs.length; i++){
+    if(divs[i].className == "fa evaluation-elem-prog fa-circle ng-star-inserted"){
+    divs[i].style.color = '#8900c0';}
+}
